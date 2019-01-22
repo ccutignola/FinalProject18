7383aa95ee2464ba3ca0dc54057f65502b3d106e
ccutignola <ccutignola@ucvts.org>
2019-01-22 18:18:52 +0000

    Third commit of connect 4. Functions that still do not work

--- connect4.py
+++ connect4.py
@@ -2,28 +2,98 @@ def intro():
   """Run this to print the intro to the game"""
   print('Welcome to Connect 4!!!!!')
 
-def get_user_input():
-  user_input = int(input('input a column from 1 - 7'))
-  if 0 > user_input or user_input > 7:
-    print('Invalid! Try again!'.upper())
+#def get_user_input():
+  #user_input = int(input('input a column from 1 - 7'))
+  #if 0 > user_input or user_input > 7:
+    #print('Invalid! Try again!'.upper())
   #x = print('How many players, 1 or 2?')
   #if x!= 1 or x !=2 :
   #print('Invalid. 1 or 2 !!!!!!!!!!!!'.upper())
+DIRECTIONS = (
+    (-1, -1), (-1, 0), (-1, 1),
+    ( 0, -1),          ( 0, 1),
+    ( 1, -1), ( 1, 0), ( 1, 1),
+)
 
+ROWS = 6
+COMS = 7
 
-def create_board():
-  row1= [0,0,0,0,0,0,0]
-  row2 = [0,0,0,0,0,0,0]
-  row3 = [0,0,0,0,0,0,0]
-  row4 = [0,0,0,0,0,0,0]
-  row5 = [0,0,0,0,0,0,0]
-  row6 = [0,0,0,0,0,0,0]
+piece_none = ''
+one = 'x'
+two = 'o'
 
-  board = print ("",row6,"\n",row5,"\n",row4,"\n",row3,"\n",row2,"\n",row1)
+board = []
+
+def create_board(rows = ROWS, columns = COMS):
+  """Creates an empty Connect 4 board."""
+  board = []
+  for row in range(rows):
+    board_row = []
+    for column in range(columns):
+      board_row.append(piece_none)
+    board.append(board_row)
   return board
-board = create_board
 
+def copy_board(board):
+  """Returns a copy of the created board"""
+  rows = len(board)
+  columns = len(board[0])
+  copied = create_board(rows, columns)
 
+  for row in range(rows):
+    for column in range(columns):
+      copied[row][column] = board[row][column]
+  return copied
 
-create_board()
+def print_board(board):
+  """Prints the Connect 4 board"""
+  for row in board:
+    print('|' + '|'.join(row) + '|')
+  return board
+
+def drop_piece(board, column, piece):
+  """Allows the user to drop a piece into the board in a specific column"""
+  for row in reversed(board):
+    if row[column] == piece_none:
+      row[column] = piece
+      return True
+  return False
+
+def find_winner(board, length=4):
+  rows = len(board)
+  columns = len(board[0])
+  for row in range(rows):
+    for column in range(columns):
+      if board[row][column] == piece_none:
+        continue
+      if check_piece(board, row, column, length):
+        return board[row][column]
+  return None
+
+def check_piece(board, row, column, length):
+  rows    = len(board)
+  columns = len(board[0])
+
+  for dr, dc in DIRECTIONS:
+    found_winner = True
+
+    for i in range(1, length):
+      r = row + dr*i
+      c = column + dc*i
+
+      if r not in range(rows) or c not in range(columns):
+        found_winner = False
+        break
+
+      if board[r][c] != board[row][column]:
+       found_winner = False
+       break
+
+    if found_winner:
+      return True
+
+  return False
 #intro()
+create_board()
+copy_board(board)
+print_board(board)
